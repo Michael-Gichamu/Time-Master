307ec4d33523c635152564c76bb56fb0e1b69d06
Michael-Gichamu <michaelmatere606@gmail.com>
2024-01-15 22:31:26 +0300

    Add: Componets of Landing Page

--- landing_page.js
+++ landing_page.js
@@ -2,7 +2,8 @@ const toggleBtn = document.querySelector('.toggle-btn');
 const toggleBtnIcon = document.querySelector('.toggle-btn i');
 const dropDownMenu = document.querySelector('.dropdown-menu');
 const header = document.querySelector('#header');
-const defaultMarginBottom = header.style.marginBottom
+const defaultMarginBottom = header.style.marginBottom;
+const welcomeH1 = document.querySelector('.welcome-content h1');
 
 toggleBtn.onclick = function() {
   dropDownMenu.classList.toggle('open');
@@ -14,5 +15,19 @@ toggleBtn.onclick = function() {
   // restore to default margin bottom.
   } else if (toggleBtnIcon.className == 'fa-solid fa-bars') {
     header.style.marginBottom = defaultMarginBottom;
+    // reset width when closing dropdown
+    welcomeH1.style.width = 'unset';
+  } else if (window.innerWidth > 600 && window.innerWidth < 992 ) {
+    //fix width of welcome h1 when dropdown is open
+    welcomeH1.style.width = '50%';
   }
 };
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 600 && toggleBtnIcon.className == 'fa-solid fa-xmark') {
+    header.style.marginBottom = defaultMarginBottom;
+    toggleBtnIcon.classList = 'fa-solid fa-bars';
+    dropDownMenu.classList.toggle('open');
+    welcomeH1.style.width = 'unset';
+  }
+})
