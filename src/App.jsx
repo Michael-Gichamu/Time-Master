af24c694ee57cf85b84bd09b6b5b4449ce21d06c
Michael-Gichamu <michaelmatere606@gmail.com>
2024-01-24 22:21:48 +0300

    Add: TimeTrackingProjectOverview webpage: Welcome section and project Overview heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Nav from "./components/Nav"
-import TimeTrackingProjectOverview from "./pages/TimeTrackingProjectOverview"
+import TimeTrackingProjectOverview from "./pages/TimeTrackingProjectOverview/TimeTrackingProjectOverview"
 
 export default function App() {
   return (
