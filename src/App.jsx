5e76151706284ef48d11c1c44df68ae9771d4214
Michael-Gichamu <michaelmatere606@gmail.com>
2024-01-27 15:48:54 +0300

    Add: Blur circle on page view UX

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ export default function App() {
     <main>
       <Nav />
       <TimeTrackingProjectOverview />
+      <div className="blur-circle circle-1"></div>
+      <div className="blur-circle circle-2"></div>
     </main>
   )
 }
