5c8032ad034d021746a4c16c9d09459c3cebdb78
Michael-Gichamu <michaelmatere606@gmail.com>
2024-01-29 09:11:12 +0300

    Add: project section and completion status bar, Added flowbite to app

--- src/pages/TimeTrackingProjectOverview/sections/ProjectOverview.jsx
+++ src/pages/TimeTrackingProjectOverview/sections/ProjectOverview.jsx
@@ -40,23 +40,32 @@ const ProjectOverview = () => {
   }
 
   return (
-    <div className="text-center my-14">
-      <h1 className="font-bold">Project Overview</h1>
+    <div className="my-14">
+      <h1 className="font-bold text-center">Project Overview</h1>
       <div className="section-container">
         <div className="section-line">
           <div>
             <Dashboard />
           </div>
           <div className="flex justify-between gap-2 ml-2 pl-2 py-4 px-7">
-            <div className="bg-[#19181B] basis-2/3">
+            <div className="bg-[#19181B] basis-3/4 px-6 py-8">
               <p>Projects</p>
-              <p>{Projects.length}</p>
+              {/* <p>{Projects.length}</p> */}
+              <div className="bg-black rounded-md my-8 py-5 px-3">
+                <div className="flex justify-between">
+                  <p>TimeMaster Design</p>
+                  <p>7hrs 31min</p>
+                </div>
+                <div className="bg-[#1E6575] mt-3 w-1/3 h-2 rounded-md flex items-center">
+                  <div className="bg-[#97C43B] h-1.5 w-3/4 mx-0.5 rounded-md"></div>
+                </div>
+              </div>
             </div>
             <div className="bg-[#19181B] px-2 py-4">
-              <p className="pb-4 font-bold">Completed</p>
+              <p className="pb-4 font-bold text-center">Completed</p>
               <div>
                 <Doughnut data={doughnutData}  options={doughnutOptions}/>
-                <p className="relative bottom-44 mx-auto font-extrabold text-5xl text-[#97C43B] ">{cumulativeCompletionStatus.toFixed(1)}%</p>
+                <p className="relative bottom-36 mx-auto font-extrabold text-5xl text-[#97C43B] ">{cumulativeCompletionStatus.toFixed(1)}%</p>
               </div>
             </div>
           </div>
