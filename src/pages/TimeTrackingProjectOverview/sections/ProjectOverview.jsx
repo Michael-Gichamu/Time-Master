592036821bbec8ed264d54c0a9104941765b0092
Michael-Gichamu <michaelmatere606@gmail.com>
2024-01-31 13:46:19 +0300

    Add: scrollbar on project feature. cumulativeCompletionStatus in Doughnut chart

--- src/pages/TimeTrackingProjectOverview/sections/ProjectOverview.jsx
+++ src/pages/TimeTrackingProjectOverview/sections/ProjectOverview.jsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react";
-import Dashboard from "../../../components/Dashboard";
+import Dashboard from "../components/Dashboard";
 import { Projects } from "../../../constants";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
-import { faWheatAwnCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+// import { faWheatAwnCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import DoughnutChart from "../components/DoughnutChart";
 
 const ProjectOverview = () => {
-  ChartJS.register(ArcElement, Tooltip, Legend);
-
   const [cumulativeCompletionStatus, setCumulativeCompletionStatus] = useState(0);
 
   useEffect(() => {
@@ -16,29 +13,8 @@ const ProjectOverview = () => {
     setCumulativeCompletionStatus(totalCompletion / 2);
   }, [Projects]);
 
-  const doughnutData = {
-    labels: ["Completed", "Remaining"],
-    datasets: [
-      {
-        data: [cumulativeCompletionStatus, 100 - cumulativeCompletionStatus],
-        backgroundColor: ["#97C43B", "#1E6575"],
-        borderColor: "#1E6575",
-      },
-    ],
-  };
-
-  const doughnutOptions = {
-    cutout: "80%", // Adjust the size of the doughnut hole,
-    radius: '80%',
-    layout: {
-      padding: {
-        top: 20,
-        left: 10,
-        right: 10,
-      },
-    },
-  }
-
+  
+  console.log(cumulativeCompletionStatus + 'from Project Overview')
   return (
     <div className="my-14">
       <h1 className="font-bold text-center">Project Overview</h1>
@@ -47,25 +23,103 @@ const ProjectOverview = () => {
           <div>
             <Dashboard />
           </div>
-          <div className="flex justify-between gap-2 ml-2 pl-2 py-4 px-7">
-            <div className="bg-[#19181B] basis-3/4 px-6 py-8">
-              <p>Projects</p>
+          <div className="md:flex justify-between gap-2 ml-2 pl-2 py-4 px-7  h-96">
+            <div className="bg-[#19181B] basis-3/4 px-6 pt-4">
+              <p className="font-bold">Projects</p>
               {/* <p>{Projects.length}</p> */}
-              <div className="bg-black rounded-md my-8 py-5 px-3">
-                <div className="flex justify-between">
-                  <p>TimeMaster Design</p>
-                  <p>7hrs 31min</p>
+              <div className="h-5/6 overflow-auto scrollbar-thin scrollbar-thumb-[#1F1F1F] scrollbar-track-[#101010] p-2">
+                <div className="bg-[#101010] rounded-md my-3 py-5 px-3">
+                  <div className="flex justify-between">
+                    <p>TimeMaster Design</p>
+                    <p>7hrs 31min</p>
+                  </div>
+                  <div className="group relative bg-[#1E6575] mt-3 w-1/3 h-2 rounded-md flex items-center">
+                    <div className="bg-[#97C43B] h-1.5 w-3/4 mx-0.5 rounded-md"></div>
+                    <div
+                     className="bg-[#1F1F1F] absolute left-full top-1/2 z-20 ml-3 -translate-y-1/2 whitespace-nowrap rounded-[5px] py-1 px-3.5 text-sm text-white opacity-0 group-hover:opacity-100"
+                     >
+                     <span
+                        className="bg-[#1F1F1F] absolute left-[-3px] top-1/2 -z-10 h-2 w-2 -translate-y-1/2 rotate-45"
+                        ></span>
+                        Completed: 75%
+                    </div>
+                  </div>
                 </div>
-                <div className="bg-[#1E6575] mt-3 w-1/3 h-2 rounded-md flex items-center">
-                  <div className="bg-[#97C43B] h-1.5 w-3/4 mx-0.5 rounded-md"></div>
+                <div className="bg-[#101010] rounded-md my-3 py-5 px-3">
+                  <div className="flex justify-between">
+                    <p>TimeMaster Design</p>
+                    <p>7hrs 31min</p>
+                  </div>
+                  <div className="group relative bg-[#1E6575] mt-3 w-1/3 h-2 rounded-md flex items-center">
+                    <div className="bg-[#97C43B] h-1.5 w-3/4 mx-0.5 rounded-md"></div>
+                    <div
+                     className="bg-[#1F1F1F] absolute left-full top-1/2 z-20 ml-3 -translate-y-1/2 whitespace-nowrap rounded-[5px] py-1 px-3.5 text-sm text-white opacity-0 group-hover:opacity-100"
+                     >
+                     <span
+                        className="bg-[#1F1F1F] absolute left-[-3px] top-1/2 -z-10 h-2 w-2 -translate-y-1/2 rotate-45"
+                        ></span>
+                        Completed: 75%
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-[#101010] rounded-md my-3 py-5 px-3">
+                  <div className="flex justify-between">
+                    <p>TimeMaster Design</p>
+                    <p>7hrs 31min</p>
+                  </div>
+                  <div className="group relative bg-[#1E6575] mt-3 w-1/3 h-2 rounded-md flex items-center">
+                    <div className="bg-[#97C43B] h-1.5 w-3/4 mx-0.5 rounded-md"></div>
+                    <div
+                     className="bg-[#1F1F1F] absolute left-full top-1/2 z-20 ml-3 -translate-y-1/2 whitespace-nowrap rounded-[5px] py-1 px-3.5 text-sm text-white opacity-0 group-hover:opacity-100"
+                     >
+                     <span
+                        className="bg-[#1F1F1F] absolute left-[-3px] top-1/2 -z-10 h-2 w-2 -translate-y-1/2 rotate-45"
+                        ></span>
+                        Completed: 75%
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-[#101010] rounded-md my-3 py-5 px-3">
+                  <div className="flex justify-between">
+                    <p>TimeMaster Design</p>
+                    <p>7hrs 31min</p>
+                  </div>
+                  <div className="group relative bg-[#1E6575] mt-3 w-1/3 h-2 rounded-md flex items-center">
+                    <div className="bg-[#97C43B] h-1.5 w-3/4 mx-0.5 rounded-md"></div>
+                    <div
+                     className="bg-[#1F1F1F] absolute left-full top-1/2 z-20 ml-3 -translate-y-1/2 whitespace-nowrap rounded-[5px] py-1 px-3.5 text-sm text-white opacity-0 group-hover:opacity-100"
+                     >
+                     <span
+                        className="bg-[#1F1F1F] absolute left-[-3px] top-1/2 -z-10 h-2 w-2 -translate-y-1/2 rotate-45"
+                        ></span>
+                        Completed: 75%
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-[#101010] rounded-md my-3 py-5 px-3">
+                  <div className="flex justify-between">
+                    <p>TimeMaster Design</p>
+                    <p>7hrs 31min</p>
+                  </div>
+                  <div className="group relative bg-[#1E6575] mt-3 w-1/3 h-2 rounded-md flex items-center">
+                    <div className="bg-[#97C43B] h-1.5 w-3/4 mx-0.5 rounded-md"></div>
+                    <div
+                     className="bg-[#1F1F1F] absolute left-full top-1/2 z-20 ml-3 -translate-y-1/2 whitespace-nowrap rounded-[5px] py-1 px-3.5 text-sm text-white opacity-0 group-hover:opacity-100"
+                     >
+                     <span
+                        className="bg-[#1F1F1F] absolute left-[-3px] top-1/2 -z-10 h-2 w-2 -translate-y-1/2 rotate-45"
+                        ></span>
+                        Completed: 75%
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="bg-[#19181B] px-2 py-4">
               <p className="pb-4 font-bold text-center">Completed</p>
-              <div>
-                <Doughnut data={doughnutData}  options={doughnutOptions}/>
-                <p className="relative bottom-36 mx-auto font-extrabold text-5xl text-[#97C43B] ">{cumulativeCompletionStatus.toFixed(1)}%</p>
+              <div className="h-5/6">
+                <DoughnutChart cumulativeCompletionStatus= {cumulativeCompletionStatus.toFixed(1)}/>
+                {/* <p className="relative bottom-36 text-center mx-auto font-extrabold text-5xl text-[#97c43b] ">{cumulativeCompletionStatus.toFixed(1)}%</p> */}
               </div>
             </div>
           </div>
