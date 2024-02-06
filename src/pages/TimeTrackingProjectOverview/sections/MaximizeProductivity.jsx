8263d64b254dfb7fc2dd833b43066c5a635d77cd
Michael-Gichamu <michaelmatere606@gmail.com>
2024-02-06 22:40:33 +0300

    Add: Maximize Productivity header section

---
+++ src/pages/TimeTrackingProjectOverview/sections/MaximizeProductivity.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MaximizeProductivity = () => {
+  return (
+    <div className="my-14">
+      <h1 className="font-bold text-center">Maximize Productivity</h1>
+      <div className="section-container">
+        <div className="section-line">
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MaximizeProductivity
