d5f0bb05407cc15393fa2ff5b5982c1b97ffe962
Michael-Gichamu <michaelmatere606@gmail.com>
2024-02-13 08:19:59 +0300

    fix: directory changes

--- install_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install npm dependencies
-npm install
-
-# Install react-datepicker
-npm install react-datepicker
-
-# Install react chart
-npm install --save chart.js react-chartjs-2
