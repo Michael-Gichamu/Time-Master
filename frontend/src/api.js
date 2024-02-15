53b997d58a7a0ea299e5ca5585b1ec2a579ceee9
Michael-Gichamu <michaelmatere606@gmail.com>
2024-02-15 09:32:43 +0300

    Add: Created endpoints to send req to server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,32 +1,50 @@
 import axios from 'axios';
 const BaseUrl = 'http://localhost:5555';
 
-export const getProjects = async () => {
-  const response = await axios.get(`${BaseUrl}/projects`);
-  return response.data;
-};
-
-export const startProject = async (projectId) => {
-  const response = await axios.put(`${BaseUrl}/projects/${projectId}/start`);
-  return response.data;
-};
-
-export const endProject = async (projectId) => {
-  const response = await axios.put(`${BaseUrl}/projects/${projectId}/end`);
-  return response.data;
-}
-
+// Create a new project
 export const createProject = async (projectData) => {
   const response = await axios.post(`${BaseUrl}/projects`, projectData);
   return response.data;
 };
 
+// Retrieves all the projects from the server
+export const getProjects = async () => {
+  const response = await axios.get(`${BaseUrl}/projects`);
+  return response.data;
+};
+
+// Update a project
 export const updateProject = async (projectId, projectData) => {
   const response = await axios.put(`${BaseUrl}/projects/${projectId}`, projectData);
   return response.data;
 };
 
+// Delete a project
 export const deleteProject = async (projectId) => {
   const response = await axios.delete(`${BaseUrl}/projects/${projectId}`);
   return response.data;
 };
+
+// Set the start time for a project
+export const startProject = async (projectId) => {
+  const response = await axios.put(`${BaseUrl}/projects/${projectId}/start`);
+  return response.data;
+};
+
+// Set the end time for a project and calculate the hours taken from the start time
+export const pauseProject = async (projectId) => {
+  const response = await axios.put(`${BaseUrl}/projects/${projectId}/pause`);
+  return response.data;
+}
+
+// Sets that a project is finished and is archived
+export const finishProject = async (projectId) => {
+  const response = await axios.put(`${BaseUrl}/projects/${projectId}/finish`);
+  return response.data;
+}
+
+// Retrieves all the archived projects from the server
+export const getarchivedProjects = async () => {
+  const response = await axios.get(`${BaseUrl}/projects/archived`);
+  return response.data;
+}
