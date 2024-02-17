63e68fa1d3682cd5559bf00d5f3ef8cfef7f65fc
Michael-Gichamu <michaelmatere606@gmail.com>
2024-02-17 21:57:40 +0300

    fix: handled time summation on multiple latest request

--- backend/projectUtils.js
+++ backend/projectUtils.js
@@ -1,3 +1,4 @@
+// Description: Utility functions for updating project data when time elapses.
 const updateProjectOnTimeElapse = async (project, reqType) => {
   if (project.regularStart) {
     // Convert regularStart string to a Date object
@@ -5,48 +6,71 @@ const updateProjectOnTimeElapse = async (project, reqType) => {
 
     // Calculate hours between regularStart and current time
     const currentTime = new Date();
-    const elapsedTimeInMilliseconds = currentTime.getTime() - regularStartDate.getTime();
-    const elapsedTimeInHours = elapsedTimeInMilliseconds / (1000 * 60 * 60);
+    let elapsedTimeInMilliseconds = currentTime.getTime() - regularStartDate.getTime();
+    const regularLatestDate = new Date(project.regularLatest);
 
-    // Save the original hoursTaken
-    let currentHoursTaken = project.hoursTaken;
-
-    // Parse hoursTaken to a float, add elapsed time, and convert back to a string
-    const updatedHoursTaken = (parseFloat(project.hoursTaken) + elapsedTimeInHours).toString();
-
-    // Update project data
-    currentHoursTaken = project.hoursTaken = updatedHoursTaken;
-    
-    // check if the project is paused
+    // Check if project is paused
     if (reqType === 'pause') {
       project.regularStart = null;
       project.regularEnd = null;
-    };
+      if (project.regularLatest) {
+        elapsedTimeInMilliseconds = currentTime.getTime() - regularLatestDate;
+        project.regularLatest = null;
+      }
+    } else if (reqType === 'latest') {
+      // check if project had a request from latest
+      if (!project.regularLatest) {
+        project.regularLatest = currentTime;
+      } else {
+        elapsedTimeInMilliseconds = currentTime.getTime() - regularLatestDate;
+        project.regularLatest = currentTime;
+        console.log(elapsedTimeInMilliseconds + 'ms2');
+      }
+    }
+
+    const elapsedTimeInSeconds = Math.floor(elapsedTimeInMilliseconds / 1000);
+    // Parse hoursTaken to a float, add elapsed time, and convert back to a string
+    const updatedHoursTaken = (parseFloat(project.hoursTaken) + (elapsedTimeInSeconds / 3600)).toString();
+
+    // Save the original hoursTaken
+    let currentHoursTaken = project.hoursTaken;
+    // Update project data
+    currentHoursTaken = project.hoursTaken = updatedHoursTaken;
 
     // Save the updated project
     await project.save();
 
     // Include the elapsed time in seconds and current hoursTaken in the response
-    const elapsedTimeInSeconds = Math.round(elapsedTimeInMilliseconds / 1000);
-    const currentHoursTakenInSeconds = Math.round(currentHoursTaken * 3600);
-    let currentTimeTaken = '';
-
-    if (currentHoursTakenInSeconds < 60) {
-      currentTimeTaken = (currentHoursTaken * 3600).toFixed(2) + ' sec';
-    } else if (currentHoursTakenInSeconds > 60) {
-      currentTimeTaken = (currentHoursTaken * 60).toFixed(2) + ' min';
-    } else if (currentHoursTakenInSeconds > 3600) {
-      currentTimeTaken = (currentHoursTaken / 60).toFixed(2) + ' hrs';
-    }
-
     return {
       project,
       elapsedTimeInSeconds: `${elapsedTimeInSeconds} sec`,
-      currentHoursTaken: currentTimeTaken
+      currentHoursTaken: getCurrentTimeTaken(currentHoursTaken)
+    };
+  } else {
+    return { 
+      project,
+      message: "Project is already paused",
+      currentHoursTaken: getCurrentTimeTaken(project.hoursTaken)
     };
   }
+};
 
-  return { message: "Project is already paused" };
+// get the current time taken
+const getCurrentTimeTaken = (currentHoursTaken) => {
+  const currentHoursTakenInSeconds = Math.round(currentHoursTaken * 3600);
+  let currentTimeTaken = '';
+
+  if (currentHoursTakenInSeconds < 60) {
+    currentTimeTaken = (currentHoursTakenInSeconds).toFixed(2) + ' sec';
+  } else if (currentHoursTakenInSeconds < 3600) {
+    currentTimeTaken = (currentHoursTakenInSeconds / 60).toFixed(2) + ' min';
+  } else {
+    currentTimeTaken = (currentHoursTakenInSeconds / 3600).toFixed(2) + ' hrs';
+  }
+
+  console.log(currentTimeTaken);
+  return {currentTimeTaken: currentTimeTaken, currentHoursTaken: currentHoursTaken};
 };
 
 export default updateProjectOnTimeElapse;
+export { getCurrentTimeTaken };
